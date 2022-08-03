a24d9e4e1fd1eb3bfc1c47c0955f19d163eba038
Rana Hisham <hishamrana00@gmail.com>
2022-08-03 20:24:26 +0200

    save for now

--- app/src/main/java/com/rana/e_shoptask/Ui/ProductAdapter.kt
+++ app/src/main/java/com/rana/e_shoptask/Ui/ProductAdapter.kt
@@ -1,7 +1,43 @@
 package com.rana.e_shoptask.Ui
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.rana.e_shoptask.R
 import kotlin.reflect.KFunction1
 
-class ProductAdapter(param: KFunction1<Product, Unit>) {
+class ProductAdapter: RecyclerView.Adapter<ProductAdapter.MyViewHolder>() {
 
+
+    private var productlist: List<ProductModel>?= null
+    fun setProductList (productlist : List<ProductModel>?) {
+
+        this.productlist = productlist
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)
+    : ProductAdapter.MyViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.shopping_item, parent,false)
+
+        return MyViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ProductAdapter.MyViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        if (productlist == null) return 0
+        else return productlist?.size!!
+    }
+    class MyViewHolder(view : View) : RecyclerView.ViewHolder(view){
+        val productImage = view.productImage
+        val productTitleTv = view.productTitleTv
+        val productdescTv = view.productdescTv
+        val productpriceTv = view.productpriceTv
+    }
 }
+
+
+
--- app/src/main/java/com/rana/e_shoptask/Ui/ShoppingList.kt
+++ app/src/main/java/com/rana/e_shoptask/Ui/ShoppingList.kt
@@ -2,10 +2,24 @@ package com.rana.e_shoptask.Ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.rana.e_shoptask.R
+import androidx.recyclerview.widget.RecyclerView
 
 class ShoppingList: AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.shopping_list)
-} }
+initRecyclerView()
+}
+
+private fun initRecyclerView(){
+
+    productsRecyclerView.layoutManager = LinearLayoutManager(this)
+
+
+    }
+
+
+}
