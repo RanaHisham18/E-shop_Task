851e794dad0e60cf9e209195c5bcbdfd8c8f33be
Rana Hisham <hishamrana00@gmail.com>
2022-08-03 19:26:22 +0200

    Retrofit call error is handled (import error)

--- app/src/main/java/com/rana/e_shoptask/data/remote/Retrofit/IProductInstance.kt
+++ app/src/main/java/com/rana/e_shoptask/data/remote/Retrofit/IProductInstance.kt
@@ -1,11 +1,11 @@
 package com.rana.e_shoptask.data.remote.Retrofit
 
-import android.telecom.Call
+import retrofit2.Call
 import com.rana.e_shoptask.Ui.ProductModel
 import retrofit2.http.GET
 
 interface IProductInstance{
 
     @GET("dummyjson.com/products")
-   fun fetchProducts():  List <ProductModel>
+   fun fetchProducts():  Call  <List <ProductModel>>
 }
